fd98c5fc7f0c4263d0cfd9d582a4082b15425495
Lv Sihan <2429894846@qq.com>
2024-06-27 01:14:39 +0800

    update models

--- teacher_app/models.py
+++ teacher_app/models.py
@@ -1,41 +1,31 @@
 from django.db import models
 from django.db.models import Q
 
-Gender = models.IntegerChoices("Gender", "男 女")
-
-Title = models.IntegerChoices(
-    "Title",
-    "博士后 助教 讲师 副教授 特任教授 教授 助理研究员 特任副研究员 副研究员 特任研究员 研究员",
-)
-
-CourseKind = models.IntegerChoices("CourseKind", "本科生课程 研究生课程")
-
-ProjectKind = models.IntegerChoices(
-    "ProjectKind", "国家级项目 省部级项目 市厅级项目 企业合作项目 其它类型项目"
-)
-
-PAPER_KIND_CHOICES = [
-    (1, "full paper"),
-    (2, "short paper"),
-    (3, "poster paper"),
-    (4, "demo paper"),
-]
-
-PAPER_LEVEL_CHOICES = [
-    (1, "CCF-A"),
-    (2, "CCF-B"),
-    (3, "CCF-C"),
-    (4, "中文 CCF-A"),
-    (5, "中文 CCF-B"),
-    (6, "无级别"),
-]
-
 
 class Teacher(models.Model):
+    GENDER_CHOICES = {
+        1: "男",
+        2: "女",
+    }
+
+    TITLE_CHOICES = {
+        1: "博士后",
+        2: "助教",
+        3: "讲师",
+        4: "副教授",
+        5: "特任教授",
+        6: "教授",
+        7: "助理研究员",
+        8: "特任副研究员",
+        9: "副研究员",
+        10: "特任研究员",
+        11: "研究员",
+    }
+
     id = models.CharField(max_length=5, primary_key=True, verbose_name="工号")
     name = models.CharField(max_length=200, verbose_name="姓名")
-    gender = models.IntegerField(choices=Gender.choices, verbose_name="性别")
-    title = models.IntegerField(choices=Title.choices, verbose_name="职称")
+    gender = models.IntegerField(choices=GENDER_CHOICES, verbose_name="性别")
+    title = models.IntegerField(choices=TITLE_CHOICES, verbose_name="职称")
 
     class Meta:
         verbose_name = "教师"
@@ -45,31 +35,66 @@ class Teacher(models.Model):
 
 
 class Course(models.Model):
+    COURSE_KIND_CHOICES = {
+        1: "本科生课程",
+        2: "研究生课程",
+    }
+
     id = models.CharField(max_length=256, primary_key=True, verbose_name="课程号")
     name = models.CharField(max_length=256, verbose_name="课程名")
     total_hours = models.PositiveIntegerField(verbose_name="总学时")
-    kind = models.IntegerField(choices=CourseKind.choices, verbose_name="课程性质")
+    kind = models.IntegerField(choices=COURSE_KIND_CHOICES, verbose_name="课程性质")
     teachers = models.ManyToManyField(Teacher, through="TeacherCourse")
+
+    class Meta:
+        verbose_name = "课程"
 
     def __str__(self):
         return f"{self.id} {self.name}"
 
 
 class Project(models.Model):
+    PROJECT_KIND_CHOICES = {
+        1: "国家级项目",
+        2: "省部级项目",
+        3: "市厅级项目",
+        4: "企业合作项目",
+        5: "其它类型项目",
+    }
+
     id = models.CharField(max_length=256, primary_key=True, verbose_name="项目号")
     name = models.CharField(max_length=256, verbose_name="项目名称")
     source = models.CharField(max_length=256, verbose_name="项目来源")
-    kind = models.IntegerField(choices=ProjectKind.choices, verbose_name="项目类型")
+    kind = models.IntegerField(choices=PROJECT_KIND_CHOICES, verbose_name="项目类型")
     total_fund = models.FloatField(verbose_name="总经费")
     start_year = models.PositiveIntegerField(verbose_name="开始年份")
     end_year = models.PositiveIntegerField(verbose_name="结束年份")
     teachers = models.ManyToManyField(Teacher, through="TeacherProject")
+
+    class Meta:
+        verbose_name = "项目"
 
     def __str__(self):
         return f"{self.id} {self.name}"
 
 
 class Paper(models.Model):
+    PAPER_KIND_CHOICES = {
+        1: "full paper",
+        2: "short paper",
+        3: "poster paper",
+        4: "demo paper",
+    }
+
+    PAPER_LEVEL_CHOICES = {
+        1: "CCF-A",
+        2: "CCF-B",
+        3: "CCF-C",
+        4: "中文 CCF-A",
+        5: "中文 CCF-B",
+        6: "无级别",
+    }
+
     id = models.PositiveIntegerField(primary_key=True, verbose_name="序号")
     title = models.CharField(max_length=256, verbose_name="论文名称")
     pub_source = models.CharField(max_length=256, verbose_name="发表源")
@@ -77,6 +102,9 @@ class Paper(models.Model):
     kind = models.IntegerField(choices=PAPER_KIND_CHOICES, verbose_name="类型")
     level = models.IntegerField(choices=PAPER_LEVEL_CHOICES, verbose_name="级别")
     teachers = models.ManyToManyField(Teacher, through="TeacherPaper")
+
+    class Meta:
+        verbose_name = "论文"
 
     def __str__(self):
         return f"{self.id} {self.title}"
@@ -114,7 +142,7 @@ class TeacherProject(models.Model):
             models.UniqueConstraint(
                 fields=["project", "rank"],
                 name="unique_project_rank",
-            )
+            ),
         ]
 
     def __str__(self):
@@ -141,7 +169,7 @@ class TeacherPaper(models.Model):
                 fields=["paper"],
                 condition=Q(corresp=True),
                 name="unique_paper_corresp",
-            )
+            ),
         ]
 
     def __str__(self):
