fb333a16085d0bc4cd07ea27ec428b8d4a0d2931
Lv Sihan <2429894846@qq.com>
2024-07-08 11:40:57 +0800

    dockerize

--- config/settings.py
+++ config/settings.py
@@ -17,18 +17,16 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-74r5ul8t$e%&osg!m!70(1b^%4d$jetzyu3w*p0u8^+8r+$30x"
+SECRET_KEY = os.environ.get("SECRET_KEY") or "django-insecure-74r5ul8t$e%&osg!m!70(1b^%4d$jetzyu3w*p0u8^+8r+$30x"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -75,19 +73,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "config.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": os.environ.get("MYSQL_DATABASE") or "teacher_app",
-        "USER": os.environ.get("MYSQL_USER") or "root",
-        "PASSWORD": os.environ.get("MYSQL_PASSWORD") or "123456",
+        "NAME": os.environ.get("DATABASE_NAME") or "teacher_app",
+        "USER": os.environ.get("DATABASE_USER") or "teacher_app",
+        "PASSWORD": os.environ.get("DATABASE_PASSWORD") or "teacher_app",
+        "HOST": os.environ.get("DATABASE_HOST") or "127.0.0.1",
+        "PORT": os.environ.get("DATABASE_PORT") or "3306"
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -107,7 +105,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -118,7 +115,6 @@ TIME_ZONE = "Asia/Shanghai"
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
